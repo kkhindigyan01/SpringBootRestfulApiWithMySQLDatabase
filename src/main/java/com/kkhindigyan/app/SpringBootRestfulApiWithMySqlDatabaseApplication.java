4956c43f12acdcfc02f4659bcc2aa2b1fea89eed
kkhindigyan01 <kkhindigyan01@gmail.com>
2021-06-11 18:24:22 +0530

    first commit

---
+++ src/main/java/com/kkhindigyan/app/SpringBootRestfulApiWithMySqlDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.kkhindigyan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApiWithMySqlDatabaseApplication {
+
+	public static void main(String[] args) {
+		 
+		SpringApplication.run(SpringBootRestfulApiWithMySqlDatabaseApplication.class, args);
+	}
+}
